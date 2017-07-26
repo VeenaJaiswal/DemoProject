b16319c204f37e2678d0a7f01eaa81525e8fcbaa
veenajaiswal <admacbook@gmail.com>
2017-07-26 22:43:51 +0530

    test project

---
+++ CollectionView/CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CollectionView
+//
+//  Created by Arun on 28/09/14.
+//  Copyright (c) 2014 IPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CollectionView/CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by Arun on 28/09/14.
+//  Copyright (c) 2014 IPL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
